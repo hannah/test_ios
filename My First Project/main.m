0892ac9e4a5d9f1a5064cec38a83080898cc74a2
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-07-30 11:23:16 -0700

    First commit

---
+++ My First Project/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  My First Project
+//
+//  Created by H Zabarsky on 7/30/14.
+//  Copyright (c) 2014 H Zabarsky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
