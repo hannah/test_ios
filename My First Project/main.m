d3a3712cc9213e99c55123b243de7ff818637bbd
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-07-30 22:18:16 -0700

    functional guess the number game

--- My First Project/main.m
+++ My First Project/main.m
@@ -12,9 +12,29 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        int answer = 0;
+        int guess = 0;
+        int turn = 0;
+        answer = arc4random() % 100 + 1;
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Pick a number between 1-100");
+        while (guess != answer) {
+            scanf("%i", &guess);
+            
+            if ( guess > answer) {
+                NSLog(@"Lower!");
+            }
+            else if ( guess < answer) {
+                NSLog(@"Higher!");
+            }
+            else {
+                NSLog(@"Correct! The answer was %i", answer);
+                NSLog(@"It took you %i tries to get the correct answer!", turn);
+            }
+            turn++;
+
+        }
+        
         
     }
     return 0;
